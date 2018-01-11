a6da8732bcb92a1a5bdf051ffacee46b2efe75c3
Angr1st <torbenclasen@gmail.com>
2018-01-11 20:20:30 +0100

    uff corrected writing mistake. Made CSVTable into TopLevelModule

--- CSVTable.fs
+++ CSVTable.fs
@@ -1,24 +1,30 @@
-module CSVTableHandler =
-    
-    type Row ={ 
-        Name : string
-        Strasse:string
-        Ort:string
-        Alter:string 
-        }
-
-    let CreateRow (x:string) (seperator:char array) =
-        let splitString = x.Split seperator
-        if splitString.Length = 4
-            then Some { Name=splitString.[0]; Strasse=splitString.[1]; Ort=splitString.[2]; Alter=splitString.[3]}
-            else None
-
-    type CSVTable ={
-        HeaderRow:Row
-        Rows:Row list
-        LongestFirstColumnEntry:int
-        LongestSecondColumnEntry:int
-        LongestThirdColumnEntry:int
-        LongestFourthCoulumnEntry:int
+   
+type Row ={ 
+    Name : string
+    Strasse:string
+    Ort:string
+    Alter:string 
     }
+
+let CreateRow (x:string) (seperator:char array) =
+    let splitString = x.Split seperator
+    if splitString.Length = 4
+        then Some { Name=splitString.[0]; Strasse=splitString.[1]; Ort=splitString.[2]; Alter=splitString.[3]}
+        else None
+
+type ColumnLength= ColumnLength of int
+
+let CreateColumnLength (x:int) =
+    if x >= 0 
+        then Some (ColumnLength x) 
+        else None
+
+type CSVTable ={
+    HeaderRow:Row
+    Rows:Row list
+    LongestFirstColumnEntry:int
+    LongestSecondColumnEntry:int
+    LongestThirdColumnEntry:int
+    LongestFourthCoulumnEntry:int
+}
         
