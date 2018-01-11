359ec90338d575b79ff4f9613c7a546b5232824b
Angr1st <torbenclasen@gmail.com>
2018-01-11 18:34:52 +0100

    Created Row and CSVTable Type. Created CreateRow Type

--- CSVTable.fs
+++ CSVTable.fs
@@ -1,3 +1,24 @@
-module CSVTableHandler 
+module CSVTableHandler =
     
-type Row ={ Name : string; } 
+    type Row ={ 
+        Name : string
+        Strasse:string
+        Ort:string
+        Alter:string 
+        }
+
+    let CreateRow (x:string) (seperator:char array) =
+        let splitString = x.Split seperator
+        if splitString.Length = 4
+            then Some { Name=splitString.[0]; Strasse=splitString.[1]; Ort=splitString.[2]; Alter=splitString.[3]}
+            else None
+
+    type CSVTable ={
+        HeaderRow:Row
+        Rows:Row list
+        LongestFirstColumnEntry:int
+        LongestSecondColumnEntry:int
+        LongestThirdColumnEntry:int
+        LongestFourthCoulumnEntry:int
+    }
+        
