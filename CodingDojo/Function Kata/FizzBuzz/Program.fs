d4dfd87cb54701f7e329081d7070eee4c63e057f
Torben Clasen <torbenclasen@gmail.com>
2018-01-13 01:07:32 +0100

    First Commit for FizzBuzzz

--- CodingDojo/Function Kata/FizzBuzz/Program.fs
+++ CodingDojo/Function Kata/FizzBuzz/Program.fs
@@ -2,6 +2,18 @@
 
 open System
 
+type FizzBuzz=
+    |Fizz of string * int
+    |Buzz of string * int
+    |FizzBuzz of string * int * int
+    |Number of int
+
+let OneToHundred = [1 .. 100]
+
+let NewLine = "\n"
+
+
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
