c62aa93a685a9c3c0a90a7444438273d30119e35
Torben Clasen <torbenclasen@gmail.com>
2018-01-15 22:34:42 +0100

    Have I done it? Can it really handle roman numbers?

--- CodingDojo/Function Kata/RomanNumerals/Program.fs
+++ CodingDojo/Function Kata/RomanNumerals/Program.fs
@@ -2,7 +2,62 @@
 
 open System
 
+type RomanNumerals =
+    |I
+    |V
+    |X
+    |L
+    |C
+    |D
+    |M
+
+let split (x:string) =
+    [for c in x -> c]
+
+let createRomanNumeral x =
+    match x with
+    |'I' -> Some I
+    |'V' -> Some V
+    |'X' -> Some X
+    |'L' -> Some L
+    |'C' -> Some C
+    |'D' -> Some D
+    |'M' -> Some M
+    |_ -> None
+
+let cleanOutNone (x:RomanNumerals option list) =
+    List.choose id x
+
+let turnToInt (x:RomanNumerals) =
+    match x with
+    |I -> 1
+    |V -> 5
+    |X -> 10
+    |L -> 50
+    |C -> 100
+    |D -> 500
+    |M -> 1000
+
+let addOrSubstract (x:int) (y:int) =
+    if x >= y 
+        then (x+y)
+    elif x = 0 && y >=1
+        then y
+    else (x-1)
+
+let turnStringIntoInt (x:string) =
+    split x |>List.map createRomanNumeral |> cleanOutNone |> List.map turnToInt
+
+let glueTogether (x:string[]) =
+    let arrayOfIntList = Array.map (turnStringIntoInt >> (List.fold addOrSubstract 0)) x
+    arrayOfIntList
+
+let newLine = "\n"
+
+let printIntArray (x:int[]) =
+    Array.map (fun y -> y.ToString()) x |> Array.reduce (fun c v -> c + newLine + v)
+
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    printfn "%s" <| (glueTogether argv |> printIntArray)
     0 // return an integer exit code
--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Coding Dojo
 
-This Repo contains my attempt at solving all [Coding-Dojos]()
+This Repo contains my attempt at solving all [Coding-Dojos](http://ccd-school.de/coding-dojo/)
 
-ChallengeOne was solved (Run this Command to show output: "dotnet .\FSharpConsole.dll "Name;Strasse;Ort;Alter" "Peter Pan;Am Hang 5;12345 Einsam;42" "Maria Schmitz;Kölner Straße 45;50123 Köln;43" "Paul Meier;Münchener Weg 1;87654 München;65"")
+Challenge nr. 1 was solved (Run this Command to show output: "dotnet .\FSharpConsole.dll "Name;Strasse;Ort;Alter" "Peter Pan;Am Hang 5;12345 Einsam;42" "Maria Schmitz;Kölner Straße 45;50123 Köln;43" "Paul Meier;Münchener Weg 1;87654 München;65"").
+
+Challenge nr. 2 was solved just use dotnet FizzBuzz.dll after you have build the executable.
