2d3baa3d23c3ec8a286605b213ea97f33d61ba4a
Torben Clasen <torbenclasen@gmail.com>
2018-01-22 21:12:14 +0100

    trying it with ints

--- CodingDojo/Function Kata/FriendlyNumbers/Program.fs
+++ CodingDojo/Function Kata/FriendlyNumbers/Program.fs
@@ -2,6 +2,21 @@
 
 open System
 
+let return10Potenz x =
+    if x <=1
+        then 1
+    else pown 10 x
+
+let listOfZehnerPotenz = seq { for i in 1 .. 100000 -> return10Potenz i }
+
+let rec turnIntoIndividualNumbers x y=
+    let modResult = x % y
+    if modResult >= 0
+        then x
+    elif modResult >= 10
+        then 
+    
+
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
